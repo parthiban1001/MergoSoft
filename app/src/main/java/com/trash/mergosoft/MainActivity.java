ecd37bdd1169c73d2ed4d5519fcd0d939bc87cf3
Parthiban.Chelladurai <parthiban.c@appsuretechnologies.com>
2023-04-03 18:00:13 +0530

    Initial commit

---
+++ app/src/main/java/com/trash/mergosoft/MainActivity.java
@@ -0,0 +1,30 @@
+package com.trash.mergosoft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.trash.mergosoft.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Find the WebView by its unique ID
+        WebView webView = findViewById(R.id.web);
+
+        // loading http://www.google.com url in the WebView.
+        webView.loadUrl("https://wms.mergosoft.com");
+
+        // this will enable the javascript.
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
